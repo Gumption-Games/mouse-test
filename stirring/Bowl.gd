86ddff066b7c6c18e48edfeb15ae407fc804f85b
Jacob Blazusiak <jblazusi@ualberta.ca>
2020-02-07 14:23:53 -0700

    Stirring animation

--- stirring/Bowl.gd
+++ stirring/Bowl.gd
@@ -1,22 +1,34 @@
 class_name Bowl extends Area2D
 
-onready var top : Area2D = $Top # Equivalent to Line 4
-onready var right : Area2D = get_node("Right")
+onready var top : Area2D = $Top
+onready var right : Area2D = $Right
 onready var bottom : Area2D = $Bottom
 onready var left : Area2D = $Left
-
-onready var areas := [top, right, bottom, left]
+onready var sprite : Sprite = $Sprite
 
 var loop = []
 const k = 4
+var pressed = false
 var max_value = pow(2, k) - 1
 enum {TOP=1, RIGHT=2, BOTTOM=4, LEFT=8} 
 const clockwise = [[1, 2, 4, 8], [2, 4, 8, 1], [4, 8, 1, 2], [8, 1, 2, 4]]
 const counter_clockwise = [[1, 8, 4, 2], [8, 4, 2, 1], [4, 2, 1, 8], [2, 1, 8, 4]]
+var spin_count : int = 0
+var velocity : float = 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if velocity != 0.0:
+		velocity -= 0.01
+	var current = sprite.get_rotation_degrees()
+	sprite.set_rotation_degrees(current + (velocity))
+	
+func _input(event):
+	if event is InputEventMouseButton: 
+		if event.pressed == true:
+			pressed = true
+		else:
+			pressed = false
 
 func _on_Top_mouse_entered():
 	on_area_entered(TOP)
@@ -39,19 +51,30 @@ func _sum(array):
 func _clockwise(array):
 	for combo in clockwise:
 		if combo == array:
+			if spin_count < 1:
+				spin_count = 0
+			spin_count += 1
+			velocity = (spin_count * (1 + spin_count/100.0))
 			print("Clockwise")
+			
 			
 func _counter_clockwise(array):
 	for combo in counter_clockwise:
 		if combo == array:
+			if spin_count > 1:
+				spin_count = 0
+			spin_count -= 1
+			velocity = (spin_count * (1 + spin_count/100.0))
 			print("Counter Clockwise")
 
 func on_area_entered(pos):
-	loop.append(pos)
-	if loop.size() == k:
-		if _sum(loop) == max_value:
-			_clockwise(loop)
-			_counter_clockwise(loop) 
-			loop.clear()
-		else:
-			loop.clear()
+	if pressed == true:
+		loop.append(pos)
+		if loop.size() == k:
+			print(loop)
+			if _sum(loop) == max_value:
+				_clockwise(loop)
+				_counter_clockwise(loop) 
+				loop.clear()
+			else:
+				loop.clear()
